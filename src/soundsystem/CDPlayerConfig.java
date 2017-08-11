15a2786122a552be5d586c821fec27a0e7e2c3a4
Tony Xu <tony.xuf@ef.com>
2017-08-11 17:27:17 +0800

    initial commit

---
+++ src/soundsystem/CDPlayerConfig.java
@@ -0,0 +1,10 @@
+package soundsystem;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan
+@Configuration
+public class CDPlayerConfig {
+
+}
---
+++ src/soundsystem/SgtPeppers.java
@@ -0,0 +1,18 @@
+package soundsystem;
+
+import org.springframework.stereotype.Component;
+
+//@Name("lonelyHeartsClub")
+@Component("lonelyHeartsClub")
+public class SgtPeppers implements CompactDisc {
+
+	private String title = "Sgt. Pepper's Lonely Hearts Club Band";
+	private String artist = "The Beatles";
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		System.out.println("Playing " + title + " by " + artist);
+		
+	}
+
+}
